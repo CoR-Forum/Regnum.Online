6d2d05ff92e0486e33fc185296dfb4cbbea13cee
root <joshua@treudler.net>
2018-09-05 01:21:59 +0200

    changes for cor forum integration

--- scripts/leaflet-src.js
+++ scripts/leaflet-src.js
@@ -5421,7 +5421,7 @@ var Attribution = Control.extend({
 
 		// @option prefix: String = 'Leaflet'
 		// The HTML text shown before the attributions. Pass `false` to disable.
-		prefix: '<a href="mailto:webmaster@regnum.online">webmaster@regnum.online</a>'
+		prefix: '<a href="mailto:joshua-regnum@treudler.net">?</a>'
 	},
 
 	initialize: function (options) {
